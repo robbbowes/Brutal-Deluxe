130534eaf73dce0183ffe75a4374c9404529f2a0
Bowes <Robert.Bowes@IM-C.DE>
2018-12-19 12:35:57 +0100

    Add resources, create header, icons and layout.js

---
+++ src/Hoc/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Components/Header_footer/Header';
+
+const Layout = (props) => {
+        return ( 
+            <div>
+            <Header/> 
+            {props.children} 
+            </div> );
+        }
+
+export default Layout;
